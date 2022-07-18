1d4c8d81d40c398dbcd1d2d2a268105bdbbbdcbf
Nikith Kalu <nikithkalu@gmail.com>
2022-07-18 18:25:54 +1000

    Create 1.17 LAB.java

---
+++ Labs/Day 1/1.17 LAB.java
@@ -0,0 +1,8 @@
+public class LabProgram {
+    public static void main(String[] args) {
+       System.out.println("Step 1 complete");
+       System.out.println("Step 2 as well");
+       System.out.println("All steps now complete");
+    }
+ }
+ 
