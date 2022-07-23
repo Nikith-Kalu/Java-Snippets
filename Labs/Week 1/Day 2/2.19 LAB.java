8227d11701e1c27c6a7ff69f3ac4d51ccabe5537
Nikith Kalu <nikithkalu@gmail.com>
2022-07-23 14:03:46 +1000

    Update 2.19 LAB.java

---
+++ Labs/Week 1/Day 2/2.19 LAB.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      /* Type your code here. */
+   }
+}
