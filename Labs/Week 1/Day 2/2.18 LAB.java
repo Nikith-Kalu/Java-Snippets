da25af0e35ec4fa2ab36dbef8797411e34d1b5b4
Nikith Kalu <nikithkalu@gmail.com>
2022-07-23 13:57:47 +1000

    Create 2.18 LAB.java

---
+++ Labs/Week 1/Day 2/2.18 LAB.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      
+      Scanner scnr = new Scanner(System.in);
+      
+      int userNum;
+      int divNum;
+      
+      userNum = scnr.nextInt();
+      divNum = scnr.nextInt();
+      
+      userNum1 = (userNum / numDiv);
+      userNum2  = (userNum1 / numDiv);
+      userNum3 = (userNum2 / numDiv);
+      
+      System.out.print(userNum1 + " " + userNum2 + " " + userNum3);
+      
+   }
+}
