7d9f22ba9fe5db3524a18db9c38a9aef600b3f57
Nikith Kalu <nikithkalu@gmail.com>
2022-07-23 14:51:44 +1000

    Update 2.20 LAB.java

--- Labs/Week 1/Day 2/2.20 LAB.java
+++ Labs/Week 1/Day 2/2.20 LAB.java
@@ -3,21 +3,28 @@ import java.util.Scanner;
 public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int num1;
-      int num2;
-      int num3;
-      int num4;
+      float num1;
+      float num2;
+      float num3;
+      float num4;
       int intProduct;
       int intAverage;
+      float fltProduct;
+      float fltAverage;
       
-      num1 = scnr.nextInt();
-      num2 = scnr.nextInt();
-      num3 = scnr.nextInt();
-      num4 = scnr.nextInt();
+      num1 = scnr.nextFloat();
+      num2 = scnr.nextFloat();
+      num3 = scnr.nextFloat();
+      num4 = scnr.nextFloat();
       
-      intProduct = (num1 * num2 * num3 * num4);
-      intAverage = ((num1 + num2 + num3 + num4) / 4);
+      intProduct = (int) (num1 * num2 * num3 * num4);
+      intAverage = (int) ((num1 + num2 + num3 + num4) / 4);
+      fltProduct = (float) (num1 * num2 * num3 * num4);
+      fltAverage = (float) ((num1 + num2 + num3 + num4) / 4);
       
       System.out.println(intProduct + " " + intAverage);
+      System.out.printf("%.3f", fltProduct);
+      System.out.print(" ");
+      System.out.printf("%.3f", fltAverage);
    }
 }
