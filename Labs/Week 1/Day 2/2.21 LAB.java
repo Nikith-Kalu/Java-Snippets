ca7e69613138e853994f9c6f8fdbb2174600a43e
Nikith Kalu <nikithkalu@gmail.com>
2022-07-23 15:24:55 +1000

    Create 2.21 LAB.java

---
+++ Labs/Week 1/Day 2/2.21 LAB.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      int seconds;
+      int minutes;
+      int hours;
+      
+      
+      
+      
+   }
+}
