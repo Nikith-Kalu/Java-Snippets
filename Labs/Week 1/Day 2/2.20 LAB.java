3a10f2d113a782a56ec094f4185cd6742657dc89
Nikith Kalu <nikithkalu@gmail.com>
2022-07-23 14:40:34 +1000

    Update 2.20 LAB.java
    Part 1 complete

--- Labs/Week 1/Day 2/2.20 LAB.java
+++ Labs/Week 1/Day 2/2.20 LAB.java
@@ -7,7 +7,17 @@ public class LabProgram {
       int num2;
       int num3;
       int num4;
+      int intProduct;
+      int intAverage;
       
-      /* Type your code here. */
+      num1 = scnr.nextInt();
+      num2 = scnr.nextInt();
+      num3 = scnr.nextInt();
+      num4 = scnr.nextInt();
+      
+      intProduct = (num1 * num2 * num3 * num4);
+      intAverage = ((num1 + num2 + num3 + num4) / 4);
+      
+      System.out.println(intProduct + " " + intAverage);
    }
 }
