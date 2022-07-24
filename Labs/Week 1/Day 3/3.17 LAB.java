14fddc38bf3cce6db7f7e2849dff2f4a914842d3
Nikith Kalu <nikithkalu@gmail.com>
2022-07-24 18:36:45 +1000

    Update 3.17 LAB.java

--- Labs/Week 1/Day 3/3.17 LAB.java
+++ Labs/Week 1/Day 3/3.17 LAB.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
-      int seconds;
-      int minutes;
-      int hours;
-
-      
-      
+      Scanner scnr = new Scanner(System.in);
+         int seconds;
+         int minutes;
+         int hours;
+         int secondsLeft;
+         
+         seconds = scnr.nextInt();
+         hours = seconds / 3600;
+         minutes = (seconds - (hours * 3600)) / 60;
+         secondsLeft = (seconds - (minutes * 60) - (hours * 3600));
+         
+         System.out.println("Hours: " + hours);
+         System.out.println("Minutes: " + minutes);
+         System.out.println("Seconds: " + secondsLeft);
 
    }
 }
