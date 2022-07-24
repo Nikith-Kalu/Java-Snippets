b2136fa3a95788c6db1841b3160eab08830dc21f
Nikith Kalu <nikithkalu@gmail.com>
2022-07-24 13:54:11 +1000

    Create 3.9 LAB.java

---
+++ Labs/Week 1/Day 3/3.9 LAB.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      double x;
+      double y;
+      double z;
+      /* Type your code here. */
+   }
+}
