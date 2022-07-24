8ef18e34dabd4af800354dcf0511cd8b01d00e6c
Nikith Kalu <nikithkalu@gmail.com>
2022-07-24 18:22:52 +1000

    Update 3.16 LAB.java

--- Labs/Week 1/Day 3/3.16 LAB.java
+++ Labs/Week 1/Day 3/3.16 LAB.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
-      /* Type your code here. */
+      Scanner scnr = new Scanner(System.in);
+      
+      double mileGallon;
+      double dollarGallon;
+      double gasCost20;
+      double gasCost75;
+      double gasCost500;
+      
+      mileGallon = scnr.nextDouble();
+      dollarGallon = scnr.nextDouble();
+      gasCost20 = (dollarGallon / mileGallon) * 20;
+      gasCost75 = (dollarGallon / mileGallon) * 75;
+      gasCost500 = (dollarGallon / mileGallon) * 500;
+      
+      System.out.printf("%.2f %.2f %.2f", gasCost20, gasCost75, gasCost500);
    }
 }
