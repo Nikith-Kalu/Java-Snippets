27c976831d6fd7fc64348c4d7fa30e0938f13f69
Nikith Kalu <nikithkalu@gmail.com>
2022-07-24 14:17:53 +1000

    Update 3.10 LAB.java
    formatting issue

--- Labs/Week 1/Day 3/3.10 LAB.java
+++ Labs/Week 1/Day 3/3.10 LAB.java
@@ -4,11 +4,17 @@ public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in); 
       double caffeineMg; // "double" supports floating-point like 75.5, versus int for integers like 75. 
+      double caf1;
+      double caf2;
+      double caf3;
       
       caffeineMg = scnr.nextDouble(); 
+      caf1 = (caffeineMg / 2);
+      caf2 = (caffeineMg / 4);
+      caf3 = (caffeineMg / 16);
       
-      System.out.println("After " + 6 + " hours: " + (caffeineMg / 2) + " mg");
-      System.out.println("After " + 12 + " hours: " + (caffeineMg / 4) + " mg");
-      System.out.println("After " + 24 + " hours: " + (caffeineMg / 16) + " mg");
+      System.out.printf("After 6 hours: %.2f mg\n", caf1);
+      System.out.printf("After 12 hours: %.2f mg\n", caf2);
+      System.out.printf("After 6 hours: %.2f mg\n", caf3);
    }
 }
