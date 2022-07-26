b5b2a50abb28aba8097750ed47cd550e54eb1c47
Nikith Kalu <nikithkalu@gmail.com>
2022-07-26 17:14:31 +1000

    Update DomainNamePrinter.java

--- Labs/Week 2/Snippets/NestedLoop/DomainNamePrinter.java
+++ Labs/Week 2/Snippets/NestedLoop/DomainNamePrinter.java
@@ -1,5 +1,23 @@
-package Labs.Week 2.Snippets.NestedLoop;
+
+import java.util.Scanner;
 
 public class DomainNamePrinter {
-    
+
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      char letter1;
+      char letter2;
+
+      System.out.println("Two-letter domain names:");
+
+      letter1 = 'a';
+      while (letter1 <= 'z') {
+         letter2 = 'a';
+         while (letter2 <= 'z') {
+            System.out.println("" + letter1 + "" + letter2 + ".com");
+            ++letter2;
+         }
+         ++letter1;
+      }
+   }
 }
