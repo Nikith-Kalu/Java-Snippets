f7eea78663b8f3445cd0c2ae0caf610480ae92b4
Nikith Kalu <nikithkalu@gmail.com>
2022-07-26 15:49:34 +1000

    Update GCDCalc.java

--- Labs/Week 2/Snippets/WhileLoop/GCDCalc.java
+++ Labs/Week 2/Snippets/WhileLoop/GCDCalc.java
@@ -1,5 +1,28 @@
-package Labs.Week 2.Snippets.WhileLoop;
+import java.util.Scanner;
+
+// Output GCD of user-input numA and numB
 
 public class GCDCalc {
-    
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int numA;  // User input
+      int numB;  // User input
+
+      System.out.print("Enter first positive integer: ");
+      numA = scnr.nextInt();
+
+      System.out.print("Enter second positive integer: ");
+      numB = scnr.nextInt();
+
+      while (numA != numB) { // Euclid's algorithm
+         if (numB > numA) {
+            numB = numB - numA;
+         }
+         else {
+            numA = numA - numB;
+         }
+      }
+
+      System.out.println("GCD is: " + numA);
+   }
 }
