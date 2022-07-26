f092b4e76bc6e561a5a5f318c2e5b1dc77a2220e
Nikith Kalu <nikithkalu@gmail.com>
2022-07-26 17:11:52 +1000

    Update CountLetters.java

--- Labs/Week 2/Snippets/ForLoop/CountLetters.java
+++ Labs/Week 2/Snippets/ForLoop/CountLetters.java
@@ -1,5 +1,22 @@
-package Labs.Week 2.Snippets.ForLoop;
+import java.util.Scanner;
 
 public class CountLetters {
-    
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      String inputWord;
+      int numLetters;
+      int i;
+
+      System.out.print("Enter a word: ");
+      inputWord = scnr.next();
+
+      numLetters = 0;
+      for (i = 0; i < inputWord.length(); ++i) {
+         if (Character.isLetter(inputWord.charAt(i))) {
+            numLetters += 1;
+         }
+      }
+
+      System.out.println("Number of letters: " + numLetters);
+   }
 }
