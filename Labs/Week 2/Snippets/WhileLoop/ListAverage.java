c4bd52c0d2c7a446323751a0f2ebe78c9351008d
Nikith Kalu <nikithkalu@gmail.com>
2022-07-26 16:00:16 +1000

    Update ListAverage.java

--- Labs/Week 2/Snippets/WhileLoop/ListAverage.java
+++ Labs/Week 2/Snippets/WhileLoop/ListAverage.java
@@ -1,5 +1,27 @@
-package Labs.Week 2.Snippets.WhileLoop;
+import java.util.Scanner;
+
+// Outputs average of list of positive integers
+// List ends with 0 (sentinel)
+// Ex: 10 1 6 3 0  yields (10 + 1 + 6 + 3) / 4, or 5
 
 public class ListAverage {
-    
+   public static void main(String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      int currValue;
+      int valuesSum;
+      int numValues;
+      
+      valuesSum = 0;
+      numValues = 0;
+      
+      currValue = scnr.nextInt();
+      
+      while (currValue > 0) { // Get values until 0 (or less)
+         valuesSum = valuesSum + currValue;
+         numValues = numValues + 1;
+         currValue = scnr.nextInt();
+      }
+
+      System.out.println("Average: " + (valuesSum / numValues));
+   }
 }
