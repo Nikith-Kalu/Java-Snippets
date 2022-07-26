d2e6b6ed8c03f68d5dbb57803a177e0dd6c0ac02
Nikith Kalu <nikithkalu@gmail.com>
2022-07-26 16:28:36 +1000

    Update ListAverage.java

--- Labs/Week 2/Snippets/ForLoop/ListAverage.java
+++ Labs/Week 2/Snippets/ForLoop/ListAverage.java
@@ -1,5 +1,22 @@
-package Labs.Week 2.Snippets.ForLoop;
+import java.util.Scanner;
 
 public class ListAverage {
-    
+   public static void main(String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      int currValue;
+      int valuesSum;
+      int numValues;
+      int i;
+      
+      numValues = scnr.nextInt(); // Gets number of values in list
+      
+      valuesSum = 0;
+      
+      for (i = 0; i < numValues; ++i) {
+         currValue = scnr.nextInt(); // Gets next value in list
+         valuesSum += currValue;
+      }
+      
+      System.out.println("Average: " + (valuesSum / numValues));
+   }
 }
