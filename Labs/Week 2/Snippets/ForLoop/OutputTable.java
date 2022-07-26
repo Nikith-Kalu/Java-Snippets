64232a01b4e4e7dde54cfac0473aa5e98691af4b
Nikith Kalu <nikithkalu@gmail.com>
2022-07-26 16:56:41 +1000

    Update OutputTable.java

--- Labs/Week 2/Snippets/ForLoop/OutputTable.java
+++ Labs/Week 2/Snippets/ForLoop/OutputTable.java
@@ -1,5 +1,13 @@
-package Labs.Week 2.Snippets.ForLoop;
-
 public class OutputTable {
-    
-}
+    public static void main(String [] args) {
+       int currC;
+       double equivalentF;
+       
+       for (currC = -10; currC <= 40; currC += 5) {
+          equivalentF = (currC * 9.0 / 5.0) + 32.0;
+          
+          System.out.print(currC + " C is ");
+          System.out.println(equivalentF + " F");
+       }
+    }
+ }
