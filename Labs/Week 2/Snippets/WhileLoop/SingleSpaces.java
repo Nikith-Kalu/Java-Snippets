4e9c87eb4be27cbb031107dd2d65f8faa3bc5465
Nikith Kalu <nikithkalu@gmail.com>
2022-07-26 17:13:14 +1000

    Update SingleSpaces.java

--- Labs/Week 2/Snippets/WhileLoop/SingleSpaces.java
+++ Labs/Week 2/Snippets/WhileLoop/SingleSpaces.java
@@ -1,5 +1,20 @@
-package Labs.Week 2.Snippets.WhileLoop;
+
+import java.util.Scanner;
 
 public class SingleSpaces {
-    
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      String userText;
+
+      System.out.print("Enter sentence: ");
+      userText = scnr.nextLine();
+
+      while (userText.indexOf("  ") != -1) {
+         // At least one double-space still exists
+         // Replace all double-spaces by one space
+         userText = userText.replace("  ", " ");
+      }
+
+      System.out.print("New sentence: " + userText);
+   }
 }
