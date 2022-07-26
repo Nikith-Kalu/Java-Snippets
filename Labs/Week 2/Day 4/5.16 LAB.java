5c0cc932aeb13a31bd046b9ddce2839ead56934b
Nikith Kalu <nikithkalu@gmail.com>
2022-07-26 18:47:14 +1000

    Update 5.16 LAB.java

--- Labs/Week 2/Day 4/5.16 LAB.java
+++ Labs/Week 2/Day 4/5.16 LAB.java
@@ -1,5 +1,33 @@
-package Labs.Week 2.Day 4;
+import java.util.Scanner;
 
-public class 5.16 LAB {
-    
+public class LabProgram {
+   public static void main(String[] args) {
+      
+      Scanner scnr = new Scanner(System.in);
+      String input;
+      String character;
+      char newCharacter;
+      int i;
+      int count;
+      
+      character = scnr.next();
+      newCharacter = character.charAt(0);
+      input = scnr.nextLine();
+      
+      count = 0;
+      
+      for (i = 0; i < input.length(); ++i) {
+         if (newCharacter == input.charAt(i)) {
+            count = count + 1;
+         }
+      }
+      
+      if (count == 1) {
+         System.out.println(count + " " + character);
+      }
+      
+      else {
+      System.out.println(count + " " + character + "'s");
+       } 
+   }
 }
