c147bec60ee52716ce770d37cc853207aac9511b
Nikith Kalu <nikithkalu@gmail.com>
2022-07-27 14:19:53 +1000

    Update ArrayPrinter.java

--- Labs/Week 2/Snippets/Arrays/ArrayPrinter.java
+++ Labs/Week 2/Snippets/Arrays/ArrayPrinter.java
@@ -1,5 +1,22 @@
-package Labs.Week 2.Snippets.Arrays;
+import java.util.Scanner;
 
 public class ArrayPrinter {
-    
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      final int NUM_ELEMENTS = 8;             // Number of elements in array
+      int[] userVals = new int[NUM_ELEMENTS]; // User numbers
+      int i;                                  // Loop index
+
+      System.out.println("Enter " + NUM_ELEMENTS + " integer values...");
+      for (i = 0; i < userVals.length; ++i) {
+         userVals[i] = scnr.nextInt();
+         System.out.println("Value: " + userVals[i]);
+      }
+      
+      System.out.print("You entered: ");
+      for (i = 0; i < userVals.length; ++i) {
+         System.out.print(userVals[i] + " ");
+      }
+      System.out.println();
+   }
 }
