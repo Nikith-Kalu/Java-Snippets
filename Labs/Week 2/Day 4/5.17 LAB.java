e27197a4964aa014e2c8efcda1a361a533c295b4
Nikith Kalu <nikithkalu@gmail.com>
2022-07-27 13:36:47 +1000

    Update 5.17 LAB.java

--- Labs/Week 2/Day 4/5.17 LAB.java
+++ Labs/Week 2/Day 4/5.17 LAB.java
@@ -1,3 +1,20 @@
-public class 5.17 LAB {
-    
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      
+      Scanner scnr = new Scanner (System.in);
+      String input;
+      int i;
+      String input1 = "";
+      
+      input = scnr.nextLine();
+      
+      for (i = 0; i < input.length(); ++i) {
+         if (Character.isLetter(input.charAt(i))) {
+            input1 += input.charAt(i);
+         }
+      }
+      System.out.println(input1);
+   }
 }
