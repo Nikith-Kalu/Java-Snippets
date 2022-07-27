7574649823049255439375e3b8fd5fd72becd7f9
Nikith Kalu <nikithkalu@gmail.com>
2022-07-27 14:05:55 +1000

    Update OldestPeople.java

--- Labs/Week 2/Snippets/Arrays/OldestPeople.java
+++ Labs/Week 2/Snippets/Arrays/OldestPeople.java
@@ -1,5 +1,24 @@
-package Labs.Week 2.Snippets.Arrays;
+
+import java.util.Scanner;
 
 public class OldestPeople {
-    
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int[] oldestPeople = new int[5]; 
+      int nthPerson;                   // User input, Nth oldest person
+
+      oldestPeople[0] = 122; // Died 1997 in France
+      oldestPeople[1] = 119; // Died 1999 in U.S.
+      oldestPeople[2] = 117; // Died 1993 in U.S.
+      oldestPeople[3] = 117; // Died 1998 in Canada
+      oldestPeople[4] = 116; // Died 2006 in Ecuador
+
+      System.out.print("Enter N (1-5): ");
+      nthPerson = scnr.nextInt();
+
+      if ((nthPerson >= 1) && (nthPerson <= 5)) {
+         System.out.print("The " + nthPerson + "th oldest person lived ");
+         System.out.println(oldestPeople[nthPerson - 1] + " years.");
+      }
+   }
 }
