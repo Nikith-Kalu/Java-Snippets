7ec5f8da8cfb8b8aa65b58076bded5abfd0722ee
Nikith Kalu <nikithkalu@gmail.com>
2022-07-27 13:37:36 +1000

    Update 5.18 LAB.java

--- Labs/Week 2/Day 4/5.18 LAB.java
+++ Labs/Week 2/Day 4/5.18 LAB.java
@@ -1,3 +1,21 @@
-public class 5.18 LAB {
-    
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner (System.in);
+      String userPassword;
+      String userPasswordMod = "";
+      String exclamationMark = "!";
+      
+      userPassword = scnr.nextLine();
+      
+      userPasswordMod = userPassword.replace("i", "1");
+      userPasswordMod = userPasswordMod.replace("a", "@");
+      userPasswordMod = userPasswordMod.replace("m", "M");
+      userPasswordMod = userPasswordMod.replace("B", "8");
+      userPasswordMod = userPasswordMod.replace("s", "$");
+      
+      userPasswordMod = userPasswordMod.concat(exclamationMark);
+      System.out.println(userPasswordMod);
+   }
 }
