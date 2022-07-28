c26ca3a797b3fb2197decaa1fcf434d39b0972a7
Nikith Kalu <nikithkalu@gmail.com>
2022-07-28 19:56:20 +1000

    Update 6.13 LAB.java

--- Labs/Week 2/Day 5/6.13 LAB.java
+++ Labs/Week 2/Day 5/6.13 LAB.java
@@ -1,5 +1,20 @@
-package Labs.Week 2.Day 5;
+import java.util.Scanner;
 
-public class 6.13 LAB {
-    
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);  
+      int numElements; 
+      int i;
+      
+      numElements = scnr.nextInt();
+      int arrayNumbers[] = new int[numElements];  
+      for (i = 0; i < numElements; ++i) {
+         arrayNumbers[i] = scnr.nextInt();
+      }
+      
+      for (i = (numElements - 1); i >= 0; --i) {
+         System.out.print(arrayNumbers[i] + ",");
+      }
+      System.out.println();
+   }
 }
