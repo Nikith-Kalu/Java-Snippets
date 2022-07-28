83540ec5721f7e16ec99ed6987468616b40ff925
Nikith Kalu <nikithkalu@gmail.com>
2022-07-28 20:02:28 +1000

    Update 6.15 LAB.java

--- Labs/Week 2/Day 5/6.15 LAB.java
+++ Labs/Week 2/Day 5/6.15 LAB.java
@@ -1,5 +1,29 @@
-package Labs.Week 2.Day 5;
+import java.util.Scanner; 
 
-public class 6.15 LAB {
-    
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int arraySize;
+      int i;
+      int threshold;
+      
+      arraySize = scnr.nextInt();
+      
+      int arrayStore[] = new int [arraySize];
+      
+      for (i=0; i < arraySize; ++i) {
+         arrayStore[i] = scnr.nextInt();
+      }
+       
+      threshold = scnr.nextInt();
+      
+      for (i=0; i<arraySize; ++i) {
+         if (arrayStore[i] < threshold) {
+            System.out.print(arrayStore[i] + ",");
+         }
+      }
+      
+      System.out.println();
+      
+   }
 }
