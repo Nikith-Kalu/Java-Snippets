612f0f8c5f4990d5922cc99159e8f1ff0cc68486
Nikith Kalu <nikithkalu@gmail.com>
2022-07-28 20:26:44 +1000

    sads

--- Labs/Week 2/Day 5/6.16 LAB.java
+++ Labs/Week 2/Day 5/6.16 LAB.java
@@ -1,82 +1,38 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class PlayerRoster {
+public class LabProgram {
    public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      int[][] players = new int[5][2];
-      boolean keepAlive = true;
-      char input;
+      Scanner scnr = new Scanner(System.in);
+      int i;
+      int Value;
+      double largeNum;
       
-      for (int i = 0; i < 5; i++) {
-         System.out.println("Enter player " + (i+1) + "'s jersey number:");
-         players[i][0] = scan.nextInt();
-         System.out.println("Enter player " + (i+1) + "'s rating:");
-         players[i][1] = scan.nextInt();
-         System.out.println();
-      }
+      Value = scnr.nextInt();
       
-      outputRoster(players, 0);
-      
-      while (keepAlive) {
-         outputMenu();
-         input = scan.next().charAt(0);
-         if (input == 'q') {
-            keepAlive = false;
-            break;
-         } else if (input == 'o') {
-            outputRoster(players, 0);
-         } else if (input == 'u') {
-            System.out.println("Enter a jersey number:");
-            int jerseyNum = scan.nextInt();
-            System.out.println("Enter a new rating for the player:");
-            int newRating = scan.nextInt();
-            for (int l = 0; l < 5; l++) {
-               if (players[l][0] == jerseyNum) {
-                  players[l][1] = newRating;
-               }
-            }
-         } else if (input == 'a') {
-            System.out.println("Enter a rating:");
-            int rating = scan.nextInt();
-            outputRoster(players, rating);
-         } else if (input == 'r') {
-            System.out.println("Enter a jersey number:");
-            int jerseyNum = scan.nextInt();
-            boolean exists = true;
-            for (int l = 0; l < 5; l++) {
-               if (players[l][0] == jerseyNum) {
-                  System.out.println("Enter a new jersey number:");
-                  players[l][0] = scan.nextInt();
-                  System.out.println("Enter a rating for the new player:");
-                  players[l][1] = scan.nextInt();
-               }
-            }
-            
-         }
-      }
-      
-      return;
-   }
+      double[] userValues = new double[Value];
    
-   public static void outputRoster(int[][] players, int min) {
-      System.out.println(((min>0) ? ("ABOVE " + min) : ("ROSTER")));
-      int item = 1;
-      for (int[] player : players) {
-         if (player[1] > min) {
-            System.out.println("Player " + item + " -- Jersey number: " + player[0] + ", Rating: " + player[1]);
-         }
-         item++;
+      for (i = 0; i < (Value); ++i) {
+         userValues[i] = scnr.nextDouble();
       }
+      
+      largeNum = userValues[0];
+      
+      for (i = 0; i < (Value); ++i) {
+         if (userValues[i] > largeNum) {
+            largeNum = userValues[i];
+         }
+      }
+      
+      for (i = 0; i < (Value); ++i) {
+         userValues[i] = userValues[i]/largeNum;
+      }
+      
+      for (i = 0; i < (Value); ++i) {
+         System.out.printf("%.2f", userValues[i]);
+         System.out.print(" ");
+      }
+      
       System.out.println();
-   }
-   
-   public static void outputMenu() {
-      System.out.println("MENU");
-      System.out.println("u - Update player rating");
-      System.out.println("a - Output players above a rating");
-      System.out.println("r - Replace player");
-      System.out.println("o - Output roster");
-      System.out.println("q - Quit\n");
-      System.out.println("Choose an option:");  
+      
    }
 }
