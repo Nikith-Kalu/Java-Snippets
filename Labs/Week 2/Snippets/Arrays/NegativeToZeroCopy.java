651f18184258929f99f34ebd0ba1609f87847d01
Nikith Kalu <nikithkalu@gmail.com>
2022-07-28 17:02:26 +1000

    Update NegativeToZeroCopy.java

--- Labs/Week 2/Snippets/Arrays/NegativeToZeroCopy.java
+++ Labs/Week 2/Snippets/Arrays/NegativeToZeroCopy.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+
 public class NegativeToZeroCopy {
-    
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      final int NUM_ELEMENTS = 8;               // Number of elements
+      int[] userVals = new int[NUM_ELEMENTS];   // User numbers
+      int[] copiedVals = new int[NUM_ELEMENTS]; // New numbers
+      int i;                                    // Loop index
+
+      // Prompt user for input values
+      System.out.println("Enter " + NUM_ELEMENTS + " integer values...");
+      for (i = 0; i < userVals.length; ++i) {
+         System.out.print("Value: ");
+         userVals[i] = scnr.nextInt();
+      }
+
+      // Convert nums to newNums
+      for (i = 0; i < userVals.length; ++i) {
+         copiedVals[i] = userVals[i];
+      }
+
+      // Convert negatives to 0
+      for (i = 0; i < copiedVals.length; ++i) {
+         if (copiedVals[i] < 0) {
+            copiedVals[i] = 0;
+         }
+      }
+
+      // Print numbers
+      System.out.println("\nOriginal and new values: ");
+      for (i = 0; i < userVals.length; ++i) {
+         System.out.println(userVals[i] + " became " + copiedVals[i]);
+      }
+      System.out.println();
+   }
 }
