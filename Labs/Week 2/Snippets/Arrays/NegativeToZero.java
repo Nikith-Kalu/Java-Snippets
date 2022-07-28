d931fa36d049126355e7ba03a22d6d64ad425f2d
Nikith Kalu <nikithkalu@gmail.com>
2022-07-28 16:53:24 +1000

    Update NegativeToZero.java

--- Labs/Week 2/Snippets/Arrays/NegativeToZero.java
+++ Labs/Week 2/Snippets/Arrays/NegativeToZero.java
@@ -1,3 +1,31 @@
+
+import java.util.Scanner;
+
 public class NegativeToZero {
-    
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      final int NUM_ELEMENTS = 8;             // Number of elements
+      int[] userVals = new int[NUM_ELEMENTS]; // User numbers
+      int i;                                  // Loop index
+
+      // Prompt user to input values
+      System.out.println("Enter " + NUM_ELEMENTS + " integer values...");
+      for (i = 0; i < userVals.length; ++i) {
+         System.out.print("Value: ");
+         userVals[i] = scnr.nextInt();
+      }
+
+      // Convert negatives to 0
+      for (i = 0; i < userVals.length; ++i) {
+         if (userVals[i] < 0) {
+            userVals[i] = 0;
+         }
+      }
+
+      // Print numbers
+      System.out.print("New numbers: ");
+      for (i = 0; i < userVals.length; ++i) {
+         System.out.print(userVals[i] + " ");
+      }
+   }
 }
