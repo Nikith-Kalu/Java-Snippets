dc0fa150e14fe9389129ca3f62676908379c44ef
Nikith Kalu <nikithkalu@gmail.com>
2022-07-28 14:18:54 +1000

    Update PostageCalc.java

--- Labs/Week 2/Snippets/Arrays/PostageCalc.java
+++ Labs/Week 2/Snippets/Arrays/PostageCalc.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+ 
 public class PostageCalc {
-    
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      // Weights in ounces
+      double[] letterWeights = {1.0, 2.0, 3.0, 3.5, 4.0, 5.0, 6.0,
+                                7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0};
+      // Costs in cents (usps.com 2017)
+      int[] postageCosts = {49, 70, 91, 112, 161, 182, 203,
+                            224, 245, 266, 287, 308, 329, 350};
+      double userLetterWeight;
+      boolean foundWeight;
+      int i;
+ 
+      // Prompt user to enter letter weight
+      System.out.print("Enter letter weight (in ounces): ");
+      userLetterWeight = scnr.nextDouble();
+ 
+      // Postage costs is based on smallest letter weight greater than
+      // or equal to mailing letter weight
+      foundWeight = false;
+ 
+      for (i = 0; (i < letterWeights.length) && (!foundWeight); ++i) {
+         if( userLetterWeight <= letterWeights[i] ) {
+            foundWeight = true;
+            System.out.print("Postage for USPS first class mail is ");
+            System.out.print(postageCosts[i]);
+            System.out.println(" cents");
+         }
+      }
+ 
+      if( !foundWeight ) {
+         System.out.println("Letter is too heavy for USPS " +
+                            "first class mail.");
+      }
+   }
 }
