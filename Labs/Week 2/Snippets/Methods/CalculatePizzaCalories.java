6156d2675421e6194142243e43d87e4efec90157
Nikith Kalu <nikithkalu@gmail.com>
2022-07-31 21:34:13 +1000

    Update CalculatePizzaCalories.java

--- Labs/Week 2/Snippets/Methods/CalculatePizzaCalories.java
+++ Labs/Week 2/Snippets/Methods/CalculatePizzaCalories.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+
 public class CalculatePizzaCalories {
-    
+
+   public static double calcCircleArea(double circleDiameter) {
+      double circleRadius;
+      double circleArea;
+      double piVal = 3.14159265;
+
+      circleRadius = circleDiameter / 2.0;
+      circleArea = piVal * circleRadius * circleRadius;
+
+      return circleArea;
+   }
+
+   public static double getPizzaCalories(Scanner scnr) {
+      double pizzaDiameter;
+      double totalCalories;
+      double caloriesPerSquareInch = 16.7;    // Regular crust pepperoni pizza
+
+      System.out.print("Enter pizza's diameter (inches): ");
+      pizzaDiameter = scnr.nextDouble();
+
+      totalCalories = calcCircleArea(pizzaDiameter) * caloriesPerSquareInch;
+
+      return totalCalories;
+   }
+
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      System.out.println("Pizza has " + getPizzaCalories(scnr) + " calories.");
+      System.out.println("Pizza has " + getPizzaCalories(scnr) + " calories.");
+   }
 }
