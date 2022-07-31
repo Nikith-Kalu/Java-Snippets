8adc69406b00145f7f32dd7a8c8d9c0d0b3dd814
Nikith Kalu <nikithkalu@gmail.com>
2022-07-31 21:27:04 +1000

    Update DatePrinter.java

--- Labs/Week 2/Snippets/Methods/DatePrinter.java
+++ Labs/Week 2/Snippets/Methods/DatePrinter.java
@@ -1,3 +1,18 @@
 public class DatePrinter {
-    
-}
+    public static void printDate(int currDay, int currMonth, int currYear) {    
+       System.out.print(currMonth + "/" + currDay + "/" + currYear);
+    }
+ 
+    public static void printDate(int currDay, String currMonth, int currYear) {
+       System.out.print(currMonth + " " + currDay + ", " + currYear);
+    }
+ 
+    public static void main(String[] args) {
+       
+       printDate(30, 7, 2012);
+       System.out.println();
+       
+       printDate(30, "July", 2012);
+       System.out.println();
+    }
+ }
