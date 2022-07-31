697e2521f5f92a9556320ae8936e1b368afa15ca
Nikith Kalu <nikithkalu@gmail.com>
2022-07-31 14:37:48 +1000

    Update HeightConverter.java

--- Labs/Week 2/Snippets/Methods/HeightConverter.java
+++ Labs/Week 2/Snippets/Methods/HeightConverter.java
@@ -1,3 +1,34 @@
+
+import java.util.Scanner;
+
 public class HeightConverter {
-    
+
+   /* Converts a height in feet/inches to centimeters */
+   public static double heightFtInToCm(int heightFt, int heightIn) {
+      final double CM_PER_IN = 2.54;
+      final int IN_PER_FT = 12;
+      int totIn;
+      double cmVal;
+
+      totIn = (heightFt * IN_PER_FT) + heightIn; // Total inches
+      cmVal = totIn * CM_PER_IN;                 // Conv inch to cm
+      return cmVal;
+   }
+
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int userFt;  // User defined feet
+      int userIn;  // User defined inches
+
+      // Prompt user for feet/inches
+      System.out.print("Enter feet: ");
+      userFt = scnr.nextInt();
+
+      System.out.print("Enter inches: ");
+      userIn = scnr.nextInt();
+
+      // Output the conversion result
+      System.out.print("Centimeters: ");
+      System.out.println(heightFtInToCm(userFt, userIn));
+   }
 }
