6d7a3053d8d6176b024cb7f7dd7b53706f0e510a
Nikith Kalu <nikithkalu@gmail.com>
2022-07-31 21:33:23 +1000

    Update DatePrinterWithCheck.java

--- Labs/Week 2/Snippets/Methods/DatePrinterWithCheck.java
+++ Labs/Week 2/Snippets/Methods/DatePrinterWithCheck.java
@@ -1,3 +1,30 @@
 public class DatePrinterWithCheck {
+    public static void printDate(int currDay, int currMonth, int currYear) {
+ 
+       // Parameter error checking
+       if ((currDay < 1) || (currDay > 31)) {
+          System.out.println("Invalid day (" + currDay + "). Using 1.");
+          currDay = 1;
+       }
+ 
+       if ((currMonth < 1) || (currMonth > 12)) {
+          System.out.println("Invalid month (" + currMonth + "). Using 1.");
+          currMonth = 1;
+       }
+ 
+       // Begin method's normal behavior
+       System.out.print(currMonth + "/" + currDay + "/" + currYear);
+    }
+ 
     
-}
+    public static void main(String[] args) {
+       printDate(30, 7, 2012);
+       System.out.print("\n\n");
+ 
+       printDate(40, 7, 2012);
+       System.out.print("\n\n");
+ 
+       printDate(30, 13, 2012);
+       System.out.print("\n\n");
+    }
+ }
