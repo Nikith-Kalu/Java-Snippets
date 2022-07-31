2a7ff6df17efa31fcb5894ce954cc2bf85f95a3c
Nikith Kalu <nikithkalu@gmail.com>
2022-07-31 19:58:16 +1000

    Update HrMinToMinTestHarness.java

--- Labs/Week 2/Snippets/Methods/HrMinToMinTestHarness.java
+++ Labs/Week 2/Snippets/Methods/HrMinToMinTestHarness.java
@@ -21,3 +21,31 @@ public class HrMinToMinTestHarness {
        System.out.println("Testing completed");
     }
  }
+/*-------------------------------------------------------------------------------------------------------------------
+
+
+ public class HrMinToMinTestHarness {
+    public static double hrMinToMin(int origHours, int origMinutes) {
+       int totMinutes;      // Resulting minutes
+ 
+       totMinutes = (origHours * 60) + origMinutes;
+ 
+       return origMinutes;
+    }
+ 
+    public static void main(String[] args) {
+       System.out.println("Testing started");
+ 
+       assert (hrMinToMin(0, 0) == 0) : "Assertion (hrMinToMin(0, 0) == 0) failed";
+       assert (hrMinToMin(0, 1) == 1) : "Assertion (hrMinToMin(0, 1) == 1) failed";
+       assert (hrMinToMin(0, 99) == 99) : "Assertion (hrMinToMin(0, 99) == 99) failed";
+       assert (hrMinToMin(1, 0) == 60) : "Assertion (hrMinToMin(1, 0) == 60) failed";
+       assert (hrMinToMin(5, 0) == 300) : "Assertion (hrMinToMin(5, 0) == 300) failed";
+       assert (hrMinToMin(2, 30) == 150) : "Assertion (hrMinToMin(2, 30) == 150) failed";
+       // Many more test vectors would be typical...
+ 
+       System.out.println("Testing completed");
+    }
+ }
+
+ ------------------------------------------------------------------------------------------------------------------*/
