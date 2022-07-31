d1d5b7259e0965d4f855bf67dadc77aba6cd6fa2
Nikith Kalu <nikithkalu@gmail.com>
2022-07-31 18:32:08 +1000

    Update ShippingCost.java

--- Labs/Week 2/Snippets/Methods/ShippingCost.java
+++ Labs/Week 2/Snippets/Methods/ShippingCost.java
@@ -1,3 +1,40 @@
+import java.util.Scanner;
+
 public class ShippingCost {
-    
+   // Determine shipping tax on cost
+   public static double calcTax(double cost) {
+     return cost * 0.15;
+   }
+
+   // Determine shipping cost based on weight
+   public static double calcShippingCost(double weight) {
+      double cost;
+
+      if (weight < 1) {
+         cost = 7.88;
+      }
+      else if (weight < 6) {
+         cost = 14.32;   
+      }
+      else if (weight < 10) {
+         cost = 21.11;
+      }
+      else{
+         cost = 25.5;
+      }
+      cost = cost + calcTax(cost);
+
+      return cost;
+   }
+
+   public static void main(String args[]) {   
+      Scanner scnr = new Scanner(System.in);
+      double weightOfPackage;        // User defined package weight
+
+      System.out.print("Enter package weight: ");
+      weightOfPackage = scnr.nextDouble();
+      System.out.print("Shipping cost: $");
+      System.out.printf("%.2f", calcShippingCost(weightOfPackage));
+      System.out.println("");
+   }
 }
