e95a7648ce4703cecc8737a4a890fb026e397434
Nikith Kalu <nikithkalu@gmail.com>
2022-07-31 19:17:59 +1000

    Update LeastCommonMultiple.java

--- Labs/Week 2/Snippets/Methods/LeastCommonMultiple.java
+++ Labs/Week 2/Snippets/Methods/LeastCommonMultiple.java
@@ -1,3 +1,31 @@
-public class LeastCommonMultiple {
-    
+import java.util.Scanner;
+
+public class ListAverage {
+   // Method prompts to enter a list of values
+   public static double computeAverage(Scanner scnr, int numCount) {
+      double valuesSum = 0;
+      int currValue = 0;
+
+      for (int i = 0; i < numCount; ++i) {
+         System.out.println("Enter number: ");
+         currValue = scnr.nextInt(); 
+         valuesSum += currValue;
+      }
+
+      return valuesSum / numCount;
+   }
+   
+   public static void main(String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      int numValues;
+      double averageVal;
+      
+      System.out.println("Enter number of values: ");
+      numValues = scnr.nextInt(); 
+      averageVal = computeAverage(scnr, numValues);
+      
+      System.out.print("Average: ");
+      System.out.printf("%.3f", averageVal);
+      System.out.println("");
+   }
 }
