6929e4b37ee32aad546f749790c40fe74aa3b4d3
Nikith Kalu <nikithkalu@gmail.com>
2022-08-01 18:25:38 +1000

    Update 7.29 LAB.java

--- Labs/Week 2/Day 6/7.29 LAB.java
+++ Labs/Week 2/Day 6/7.29 LAB.java
@@ -1,5 +1,29 @@
-package Labs.Week 2.Day 6;
+import java.util.Scanner;
 
-public class 7.29 LAB {
-    
+public class LabProgram {
+	   public static int calcNumCharacters(String userString, char userChar) { 
+		      int count = 0; 
+		      
+		      for (int i = 0; i < userString.length(); i++) { 
+		         if (userString.charAt(i) == userChar) { 
+		            count++;
+		         }
+		      } 
+	
+		      return count;  
+		   }
+		   
+		   public static void main(String[] args) { 
+		      Scanner scnr = new Scanner(System.in);
+			      char userChar = scnr.next().charAt(0); 
+			      String userString = scnr.nextLine();
+			      
+			   if (calcNumCharacters(userString, userChar) == 1){
+			      System.out.println(calcNumCharacters(userString, userChar) + " " + userChar);
+			   }
+			   else {
+			      System.out.println(calcNumCharacters(userString, userChar) + " " + userChar + "'s");
+			   }
+
+		   }
 }
