41a9042d6dc6d99ae9d5c367345fa21674adde92
Nikith Kalu <nikithkalu@gmail.com>
2022-08-01 17:27:52 +1000

    Update 7.25 LAB.java

--- Labs/Week 2/Day 6/7.25 LAB.java
+++ Labs/Week 2/Day 6/7.25 LAB.java
@@ -1,5 +1,21 @@
-package Labs.Week 2.Day 6;
+import java.util.Scanner;
 
-public class 7.25 LAB {
-    
+public class Main {
+   
+   public static double kiloToPounds(double kilos) {
+      // This statement intentionally has an error. 
+      return (kilos * 2.204);
+   }
+
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      double kilos;
+      double pounds;
+      
+      kilos = scnr.nextDouble();
+      
+      pounds = kiloToPounds(kilos);
+      System.out.println(pounds + " lbs");
+   }
 }
