54af334d81b9d4405bf239e7561773a05ef4c25e
Nikith Kalu <nikithkalu@gmail.com>
2022-08-02 15:06:24 +1000

    Update O(N^2)Quadratic.java

--- Labs/Snippets/Algorithms/ONotation/O(N^2)Quadratic.java
+++ Labs/Snippets/Algorithms/ONotation/O(N^2)Quadratic.java
@@ -1,5 +1,13 @@
-package Labs.Snippets.Algorithms.ONotation;
-
-public class O(N^2)Quadratic {
-    
-}
+SelectionSort(numbers, N) { 
+    for (i = 0; i < N; ++i) {
+       indexSmallest = i
+       for (j = i + 1; j < N; ++j) {
+          if (numbers[j] < numbers[indexSmallest]) {
+             indexSmallest = j
+          }
+       }
+       
+       temp = numbers[i]
+       numbers[i] = numbers[indexSmallest]
+       numbers[indexSmallest] = temp
+ }
