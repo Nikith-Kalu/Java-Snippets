79dc7a3161ae3955ef7adf4cc57a7149013e3c60
Nikith Kalu <nikithkalu@gmail.com>
2022-08-02 14:03:39 +1000

    Update BinarySearch.java

--- Labs/Snippets/Algorithms/BinarySearch.java
+++ Labs/Snippets/Algorithms/BinarySearch.java
@@ -1,5 +1,53 @@
-package Labs.Snippets.Algorithms;
+import java.util.Scanner;
 
 public class BinarySearch {
-    
+   public static int binarySearch(int [] numbers, int key) {
+      int mid;
+      int low;
+      int high;
+      
+      low = 0;
+      high = numbers.length - 1;
+
+      while (high >= low) {
+         mid = (high + low) / 2;
+         if (numbers[mid] < key) {
+            low = mid + 1;
+         } 
+         else if (numbers[mid] > key) {
+            high = mid - 1;
+         } 
+         else {
+            return mid;
+         }
+      }
+
+      return -1; // not found
+   }
+
+   public static void main(String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      int [] numbers = {2, 4, 7, 10, 11, 32, 45, 87};
+      int i;
+      int key;
+      int keyIndex;
+
+      System.out.print("NUMBERS: ");
+      for (i = 0; i < numbers.length; ++i) {
+         System.out.print(numbers[i] + " ");
+      }
+      System.out.println();
+
+      System.out.print("Enter a value: ");
+      key = scnr.nextInt();
+
+      keyIndex = binarySearch(numbers, key);
+
+      if (keyIndex == -1) {
+         System.out.println(key + " was not found.");
+      } 
+      else {
+         System.out.println("Found " + key + " at index " + keyIndex + ".");
+      }
+   }
 }
