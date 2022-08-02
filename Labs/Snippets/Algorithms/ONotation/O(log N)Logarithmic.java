65c932569b0a1174ec885bb9ca269195c0040af7
Nikith Kalu <nikithkalu@gmail.com>
2022-08-02 15:00:35 +1000

    Update O(log N)Logarithmic.java

--- Labs/Snippets/Algorithms/ONotation/O(log N)Logarithmic.java
+++ Labs/Snippets/Algorithms/ONotation/O(log N)Logarithmic.java
@@ -1,5 +1,22 @@
-package Labs.Snippets.Algorithms.ONotation;
-
-public class O(log N)Logarithmic {
-    
+BinarySearch(numbers, N, key) {
+    mid = 0;
+   low = 0;
+   high = 0;
+   
+   high = N - 1;
+   
+   while (high >= low) {
+      mid = (high + low) / 2
+      if (numbers[mid] < key) {
+         low = mid + 1
+      }
+      else if (numbers[mid] > key) {
+         high = mid - 1
+      }
+      else {
+         return mid
+      }
+   }
+   
+   return -1   // not found
 }
