edf2651df67d0d57919fb14f2d05fffccb21fa8b
Nikith Kalu <nikithkalu@gmail.com>
2022-08-02 15:06:38 +1000

    Update O(c^N)Exponential.java

--- Labs/Snippets/Algorithms/ONotation/O(c^N)Exponential.java
+++ Labs/Snippets/Algorithms/ONotation/O(c^N)Exponential.java
@@ -1,5 +1,6 @@
-package Labs.Snippets.Algorithms.ONotation;
-
-public class O(c^N)Exponential {
-    
-}
+Fibonacci(N) {
+    if ((1 == N) || (2 == N)) {
+      return 1
+    }
+    return Fibonacci(N-1) + Fibonacci(N-2)
+  }
