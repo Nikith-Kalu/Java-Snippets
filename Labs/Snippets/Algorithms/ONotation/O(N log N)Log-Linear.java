8d8b83356b16478c3b2d26ea08044b45fcfcbbe8
Nikith Kalu <nikithkalu@gmail.com>
2022-08-02 15:05:11 +1000

    Update O(N log N)Log-Linear.java

--- Labs/Snippets/Algorithms/ONotation/O(N log N)Log-Linear.java
+++ Labs/Snippets/Algorithms/ONotation/O(N log N)Log-Linear.java
@@ -1,5 +1,10 @@
-package Labs.Snippets.Algorithms.ONotation;
-
-public class O(N log N)Log-Linear {
-    
-}
+MergeSort(numbers, i, k) {
+    j = 0
+    if (i < k) {
+       j = (i + k) / 2              // Find midpoint 
+       
+       MergeSort(numbers, i, j)     // Sort left part
+       MergeSort(numbers, j + 1, k) // Sort right part
+       Merge(numbers, i, j, k)      // Merge parts
+    }
+ }
