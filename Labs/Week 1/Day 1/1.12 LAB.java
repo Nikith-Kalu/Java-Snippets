a14cc44acdeda8375e31ad282f79a2a2231cbf14
Nikith Kalu <nikithkalu@gmail.com>
2022-08-03 23:27:22 +1000

    offloading the 'labs' folder

--- Labs/Week 1/Day 1/1.12 LAB.java
+++
@@ -1,5 +0,0 @@
-public class LabProgram {
-   public static void main(String[] args) {
-      System.out.println("Hello World!");
-   }
-}
--- Labs/Week 1/Day 1/1.17 LAB.java
+++
@@ -1,8 +0,0 @@
-public class LabProgram {
-    public static void main(String[] args) {
-       System.out.println("Step 1 complete");
-       System.out.println("Step 2 as well");
-       System.out.println("All steps now complete");
-    }
- }
- 
