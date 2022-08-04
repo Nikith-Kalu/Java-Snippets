bea6802afae56a9c669f231c8dfc617ca4382775
Nikith Kalu <nikithkalu@gmail.com>
2022-08-04 19:35:40 +1000

    Update RestaurantFavourites.java

--- Snippets/Classes/RestaurantFavourites.java
+++ Snippets/Classes/RestaurantFavourites.java
@@ -1,5 +1,16 @@
-package Snippets.Classes;
-
-public class RestaurantFavourites {
-    
-}
+public class RestaurantFavorites {
+    public static void main(String[] args) {
+       Restaurant favLunchPlace = new Restaurant();
+       Restaurant favDinnerPlace = new Restaurant();
+ 
+       favLunchPlace.setName("Central Deli");
+       favLunchPlace.setRating(4);
+ 
+       favDinnerPlace.setName("Friends Cafe");
+       favDinnerPlace.setRating(5);
+ 
+       System.out.println("My favorite restaurants: ");
+       favLunchPlace.print();
+       favDinnerPlace.print();
+    }
+ }
