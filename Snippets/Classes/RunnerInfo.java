2e033424bb037f054309684f1ea9535e02c81cbe
Nikith Kalu <nikithkalu@gmail.com>
2022-08-05 17:36:00 +1000

    Update RunnerInfo.java

--- Snippets/Classes/RunnerInfo.java
+++ Snippets/Classes/RunnerInfo.java
@@ -1,3 +1,16 @@
-public class RunnerInfo {
-    
-}
+class RunnerInfo {
+    __(A)__ int timeRun;
+    private double distRun;
+ 
+    __(B)__ void setTime(int timeRunSecs) {     // Time run in seconds
+       timeRun = timeRunSecs;                   // timeRun refers to the field
+    }
+ 
+    public void setDist(double distRunMiles) {  // Distance run in miles
+       distRun = distRunMiles;
+    }
+ 
+    public double getSpeedMph() {               // Speed in miles/hour
+       return distRun / (timeRun / 3600.0);     // miles / (secs / (secs / hour))
+    }
+ }
