42c9206b4591a158f10e3ea40e2bd697e64dd700
Nikith Kalu <nikithkalu@gmail.com>
2022-08-06 15:41:55 +1000

    Update MyRestaurant.java

--- Snippets/Classes/MyRestaurant.java
+++ Snippets/Classes/MyRestaurant.java
@@ -1,3 +1,9 @@
 public class MyRestaurant {
-    
-}
+    public static void main(String[] args) {
+       Restaurant myPlace = new Restaurant();
+       myPlace.setName("Maria's Diner");
+       myPlace.setRating(5);
+       System.out.print(myPlace.getName() + " is rated ");
+       System.out.println(myPlace.getRating());
+    }
+  }
