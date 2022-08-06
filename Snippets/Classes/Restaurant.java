c198999f6c619b3f06ce684187aefbcd0d5c3076
Nikith Kalu <nikithkalu@gmail.com>
2022-08-06 16:46:37 +1000

    Update Restaurant.java

--- Snippets/Classes/Restaurant.java
+++ Snippets/Classes/Restaurant.java
@@ -1,24 +1,27 @@
-public class Restaurant {                          
-    private String name;
-    private int rating;
- 
-    public void setName(String restaurantName) {  // Mutator
-       name = restaurantName;
-    }
- 
-    public void setRating(int userRating) {       // Mutator
-       rating = userRating;
-    }
- 
-    public String getName() {  // Accessor
-       return name;
-    }
- 
-    public int getRating() {  // Accessor
-       return rating;
-    }
- 
-    public void print() {      // Accessor
-       System.out.println(name + " -- " + rating);
-    }
- }
+import java.util.Scanner;
+
+// Review and Reviews classes omitted from the figure 
+
+public class Restaurant {
+   private String name;
+   private Reviews reviews = new Reviews();
+   
+   public void setName(String restaurantName) {
+      name = restaurantName;
+   }
+      
+   public void readAllReviews(Scanner scnr) {
+      System.out.println("Type ratings + comments. To end: -1");
+      reviews.inputReviews(scnr);
+   }
+   
+   public void printCommentsByRating() { 
+      int i;
+      
+      System.out.println("Comments for each rating level: ");
+      for (i = 1; i <= 5; ++i) {
+         System.out.println(i + ":");
+         reviews.printCommentsForRating(i);
+      }
+   }
+}
