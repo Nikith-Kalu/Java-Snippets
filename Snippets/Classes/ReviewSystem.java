939a66b22543da392ca9395e061a3f7ee717150e
Nikith Kalu <nikithkalu@gmail.com>
2022-08-06 16:46:10 +1000

    Update ReviewSystem.java

--- Snippets/Classes/ReviewSystem.java
+++ Snippets/Classes/ReviewSystem.java
@@ -5,33 +5,21 @@ public class ReviewSystem {
  
    public static void main(String [] args) {
       Scanner scnr = new Scanner(System.in);
-      ArrayList<Review> reviewList = new ArrayList<Review>();
-      Review currReview;
+      Reviews allReviews = new Reviews();
+      String currName;
       int currRating;
-      String currComment;
-      int i;
    
       System.out.println("Type rating + comments. To end: -1");
-      currRating = scnr.nextInt();
-      while (currRating >= 0) {
-         currReview = new Review();
-         currComment = scnr.nextLine(); // Gets rest of line
-         currReview.setRatingAndComment(currRating, currComment);
-         reviewList.add(currReview);
-         currRating = scnr.nextInt();
-      }
+      allReviews.inputReviews(scnr);
+   
+      System.out.println("\nAverage rating: ");
+      System.out.println(allReviews.getAverageRating());
    
       // Output all comments for given rating
-      System.out.println();
-      System.out.println("Type rating. To end: -1");
+      System.out.println("\nType rating. To end: -1");
       currRating = scnr.nextInt();
       while (currRating != -1) {
-         for (i = 0; i < reviewList.size(); ++i) {
-            currReview = reviewList.get(i);
-            if (currRating == currReview.getRating()) {
-               System.out.println(currReview.getComment());
-            }
-         }
+         allReviews.printCommentsForRating(currRating);
          currRating = scnr.nextInt();
       }
    }
