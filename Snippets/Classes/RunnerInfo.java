4a9ed1c078a77d674004b80599da45722da5a218
Nikith Kalu <nikithkalu@gmail.com>
2022-08-06 12:22:51 +1000

    Update RunnerInfo.java

--- Snippets/Classes/RunnerInfo.java
+++ Snippets/Classes/RunnerInfo.java
@@ -14,17 +14,3 @@ class RunnerInfo {
        return distRun / (timeRun / 3600.0);     // miles / (secs / (secs / hour))
     }
  }
- public class RaceResults {
-    public static void main(String[] args) {
-       RunnerInfo runner1 = new RunnerInfo();          // User-created object of class type RunnerInfo
-       RunnerInfo runner2 = new RunnerInfo(); // A second object
- 
-       runner1.setTime(360);
-       runner1.setDist(1.2);
-       runner2.setTime(200);
-       runner2.setDist(0.5);
- 
-       System.out.println("Runner1's speed in MPH: " + runner1.getSpeedMph();
-       System.out.println("Runner2's speed in MPH: " + runner2.getSpeedMph();
-    }
- }
