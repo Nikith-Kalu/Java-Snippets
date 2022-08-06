7b9e8177d2eb792cd91dbb9cf49e3ce3d3e6f6e7
Nikith Kalu <nikithkalu@gmail.com>
2022-08-06 12:21:14 +1000

    Update Restaurant.java

--- Snippets/Classes/Restaurant.java
+++ Snippets/Classes/Restaurant.java
@@ -1,3 +1,16 @@
-public class Restaurant {
-    
-}
+public class Restaurant {                          // Info about a restaurant
+    private String name;
+    private int rating;
+ 
+    public void setName(String restaurantName) {    // Sets the restaurant's name
+       name = restaurantName;
+    }
+ 
+    public void setRating(int userRating) {         // Sets the rating (1-5, with 5 best)
+       rating = userRating;
+    }
+ 
+    public void print() {                           // Prints name and rating on one line
+       System.out.println(name + " -- " + rating);
+    }
+ }
