f3c6ff86b1ff0bde74d07e09cde482112531773a
Nikith Kalu <nikithkalu@gmail.com>
2022-08-06 16:43:28 +1000

    Update Review.java

--- Snippets/Classes/Review.java
+++ Snippets/Classes/Review.java
@@ -1,3 +1,11 @@
 public class Review {
+    private int rating = -1;
+    private String comment = "NoComment";
     
-}
+    public void setRatingAndComment(int revRating, String revComment) {
+       rating = revRating;
+       comment = revComment;
+    }
+    public int getRating() { return rating; }
+    public String getComment() { return comment; }
+ }
