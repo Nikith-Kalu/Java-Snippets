70f81889acbf653ba8b37d7c6c969c2a5c2ffd7e
Nikith Kalu <nikithkalu@gmail.com>
2022-08-06 16:47:11 +1000

    Update RestaurantReviews.java

--- Snippets/Classes/RestaurantReviews.java
+++ Snippets/Classes/RestaurantReviews.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class RestaurantReviews {
-    
+ 
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      Restaurant ourPlace = new Restaurant();
+      String currName;
+   
+      System.out.println("Type restaurant name: ");
+      currName = scnr.nextLine();
+      ourPlace.setName(currName);
+      System.out.println();
+   
+      ourPlace.readAllReviews(scnr);
+      System.out.println();
+   
+      ourPlace.printCommentsByRating();
+   }
 }
