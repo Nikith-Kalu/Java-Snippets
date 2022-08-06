a170238f1caa2ac99bd423e7f71b6dd172f7eee4
Nikith Kalu <nikithkalu@gmail.com>
2022-08-06 12:22:38 +1000

    Update RaceResults.java

--- Snippets/Classes/RaceResults.java
+++ Snippets/Classes/RaceResults.java
@@ -1,3 +1,14 @@
 public class RaceResults {
-    
-}
+    public static void main(String[] args) {
+       RunnerInfo runner1 = __(C)__;          // User-created object of class type RunnerInfo
+       RunnerInfo runner2 = new RunnerInfo(); // A second object
+ 
+       runner1.setTime(360);
+       runner1.setDist(1.2);
+       runner2.setTime(200);
+       runner2.setDist(0.5);
+ 
+       System.out.println("Runner1's speed in MPH: " + runner1.__(D)__);
+       System.out.println("Runner2's speed in MPH: " + __(E)__);
+    }
+ }
