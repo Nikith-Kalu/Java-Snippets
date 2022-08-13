5a8298074a1a0c2941d9caa8dfdb3157c565859d
Nikith Kalu <nikithkalu@gmail.com>
2022-08-13 23:38:26 +1000

    Update Business.java

--- Snippets/Inheritance/12.1.2/Business.java
+++ Snippets/Inheritance/12.1.2/Business.java
@@ -1,3 +1,16 @@
 public class Business {
+    private String name;
+    private String address;
     
-}
+    public void setName(String busName) { 
+       name = busName; 
+    }
+    
+    public void setAddress(String busAddress) {
+       address = busAddress; 
+    }
+    
+    public String getDescription() {
+       return name + " -- " + address;
+    }
+ }
