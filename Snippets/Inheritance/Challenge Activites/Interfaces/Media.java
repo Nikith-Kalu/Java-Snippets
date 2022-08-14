6092c3327fbc1489f36b99914f87dd97c73f6e8e
Nikith Kalu <nikithkalu@gmail.com>
2022-08-14 14:28:45 +1000

    Update Media.java

--- Snippets/Inheritance/Challenge Activites/Interfaces/Media.java
+++ Snippets/Inheritance/Challenge Activites/Interfaces/Media.java
@@ -1,5 +1,24 @@
-package Snippets.Inheritance.Challenge Activites.Interfaces;
-
-public class Media {
-    
-}
+public class Media implements MultiMedia, Song {
+    int minutes;
+    int seconds;
+    String singer;
+ 
+    public void setDuration(int setMinutes, int setSeconds) {
+       minutes = setMinutes;
+       seconds = setSeconds;
+    }
+ 
+    public void setSinger(String setSinger) {
+       singer = setSinger;
+    }
+ 
+    @Override
+    public void printDuration() {
+       System.out.println(minutes + ":" + seconds);
+    }
+ 
+    @Override
+    public void printSinger() {
+       System.out.println("Singer: " + singer);
+    }
+ }
