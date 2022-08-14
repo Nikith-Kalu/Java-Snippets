71810a6e8f9151809007cb7aa333f97f01b92ba8
Nikith Kalu <nikithkalu@gmail.com>
2022-08-14 12:22:43 +1000

    Update ArrayListManager.java

--- Snippets/Inheritance/Challenge Activites/ArrayList of Objects/ArrayListManager.java
+++ Snippets/Inheritance/Challenge Activites/ArrayList of Objects/ArrayListManager.java
@@ -1,5 +1,26 @@
-package Snippets.Inheritance.Challenge Activites.ArrayList of Objects;
+import java.util.ArrayList;
 
 public class ArrayListManager {
-    
+   
+   public static void printArrayList(ArrayList<Object> objList) {
+      int i;
+
+      for (i = 0; i < objList.size(); ++i) {
+         System.out.println(objList.get(i));
+      }
+   }
+   
+   public static void main(String[] args) {       
+      ArrayList<Object> objList = new ArrayList<Object>();
+      
+      String myString = new String("Graph");
+      Coffee myCoffee = new Coffee("Mocha", "Dark");
+      Business myBusiness = new Business("Dinoco", "1 Park Ave");
+      
+      objList.add(myCoffee);
+      objList.add(myBusiness);
+      objList.add(myString);
+      
+      printArrayList(objList);   
+   }
 }
